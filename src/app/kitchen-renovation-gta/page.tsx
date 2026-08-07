5ef2375c0da55ec5ee565079cac750f41885cec2
unitedseocanada-cpu <unitedseocanada@gmail.com>
2026-08-07 22:08:12 +0500

    Kitchen Renovation Landing Page

---
+++ src/app/kitchen-renovation-gta/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import KitchenClient from "./KitchenClient";
+
+export const metadata = {
+  title: "Kitchen Renovation Experts in the GTA | Y2 Design & Build",
+  robots: {
+    index: false,
+    follow: true,
+  },
+};
+
+export default function KitchenRenovationGtaPage() {
+  return <KitchenClient />;
+}
